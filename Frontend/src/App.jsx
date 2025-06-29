11acdf092bc69cda5571c29439c2100d04db7b69
aGRAWAL <gargbhoomi01@gmail.com>
2025-06-29 10:42:16 +0530

    added lazy loading

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,41 +3,49 @@ import Home from "./components/Home";
 import Login from "./components/auth/login";
 import Signup from "./components/auth/signup";
 import Layout from "./components/Layout";
-import Feature from './components/Feature';
-import SavedCodes from "./components/SavedCodes";
-import NavbarComponent from "./components/Component_designs/NavbarComponent";
-import ButtonComponent from "./components/Component_designs/ButtonComponent";
-import FormComponent from "./components/Component_designs/FormComponent";
-import CardComponent from "./components/Component_designs/CardComponent";
-import ModalComponent from "./components/Component_designs/ModalComponent";
-import FlipComponent from "./components/Animation_Designs/FlipComponent";
-import Books from "./components/Animation_Designs/Books"
+import { FaSpinner } from "react-icons/fa";
+import { lazy, Suspense } from "react";
+const SavedCodes = lazy(() => import("./components/SavedCodes"));
+const NavbarComponent = lazy(() => import("./components/Component_designs/NavbarComponent"));
+const ButtonComponent = lazy(() => import("./components/Component_designs/ButtonComponent"));
+const FormComponent = lazy(() => import("./components/Component_designs/FormComponent"));
+const CardComponent = lazy(() => import("./components/Component_designs/CardComponent"));
+const ModalComponent = lazy(() => import("./components/Component_designs/ModalComponent"));
+const FlipComponent = lazy(() => import("./components/Animation_Designs/FlipComponent"));
+const Books = lazy(() => import("./components/Animation_Designs/Books"));
+const Feature = lazy(() => import("./components/Feature"));
 import { ToastProvider } from "./components/ToastProvider";
 
 function App() {
   return (
     <ToastProvider>
-    <Router>
-      <Routes>
-        {/* Routes WITHOUT layout (no navbar etc) */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+      <Router>
+        <Suspense fallback={
+          <div className="flex justify-center items-center h-screen text-indigo-600 text-2xl animate-spin">
+            <FaSpinner />
+          </div>
+        }>
+          <Routes>
+            {/* Routes WITHOUT layout */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-        {/* Routes WITH layout */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/saved-codes" element={<SavedCodes/>}/>
-          <Route path="/navbar-codes" element={<NavbarComponent/>}/>
-          <Route path="/button-codes" element={<ButtonComponent/>}/>
-          <Route path="/form-codes" element={<FormComponent/>}/>
-          <Route path="/card-codes" element={<CardComponent/>}/>
-          <Route path="/modal-codes" element={<ModalComponent/>}/>
-          <Route path="/flip-codes" element={<FlipComponent/>}/>
-          <Route path="/book-codes" element={<Books/>}/>
-          <Route path="/features" element={<Feature/>}/>
-        </Route>
-      </Routes>
-    </Router>
+            {/* Routes WITH layout */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/saved-codes" element={<SavedCodes />} />
+              <Route path="/navbar-codes" element={<NavbarComponent />} />
+              <Route path="/button-codes" element={<ButtonComponent />} />
+              <Route path="/form-codes" element={<FormComponent />} />
+              <Route path="/card-codes" element={<CardComponent />} />
+              <Route path="/modal-codes" element={<ModalComponent />} />
+              <Route path="/flip-codes" element={<FlipComponent />} />
+              <Route path="/book-codes" element={<Books />} />
+              <Route path="/features" element={<Feature />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
     </ToastProvider>
   );
 }
